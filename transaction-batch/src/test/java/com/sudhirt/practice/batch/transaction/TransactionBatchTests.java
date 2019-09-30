3f385e3f0c4727cc03dfdd6f7a57f5fa35673a4f
Sudhir Tumati <mailofsudhir@gmail.com>
2019-09-30 22:58:32 +0800

    chore: remove custom config for JobLauncherTestUtils

--- transaction-batch/src/test/java/com/sudhirt/practice/batch/transaction/TransactionBatchTests.java
+++ transaction-batch/src/test/java/com/sudhirt/practice/batch/transaction/TransactionBatchTests.java
@@ -8,14 +8,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { TransactionBatchApplication.class, TransactionBatchTests.TestConfig.class })
+@SpringBootTest
+@SpringBatchTest
 public class TransactionBatchTests {
 
 	@Autowired
@@ -29,16 +29,6 @@ public class TransactionBatchTests {
 		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 		assertThat("COMPLETED").isEqualTo(jobExecution.getExitStatus().getExitCode());
 		assertThat(9).isEqualTo(transactionEntryRepository.count());
-	}
-
-	@Configuration
-	static class TestConfig {
-
-		@Bean
-		JobLauncherTestUtils jobLauncherTestUtils() {
-			return new JobLauncherTestUtils();
-		}
-
 	}
 
 }
